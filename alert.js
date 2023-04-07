ba16c3fe5852a0de72f99bb930ade1c4fbbf1210
E-corp  Company Group Movie Lander <127004003+e-corpcompanygroupmovieslander@users.noreply.github.com>
2023-04-07 14:16:43 +0300

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,1 @@
+alert('runs on node');
---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+app.use(express.static('./'))
+const port = 3000
+
+app.get('/', (req, res) => res.sendFile('index.html',{root : __dirname}))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
