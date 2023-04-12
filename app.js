c424e2c2291acb099ff3f533d988c9516ad06bd7
E-corp  Company Group Movie Lander <127004003+e-corpcompanygroupmovieslander@users.noreply.github.com>
2023-04-12 17:12:31 +0300

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-app.use(express.static('./'))
-const port = 3000
-
-app.get('/', (req, res) => res.sendFile('index.html',{root : __dirname}))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
